55f41a5f9b149a459fe676fc036948f3d6a418e7
esspl <essplmsdn@hotmail.com>
2019-11-18 17:33:24 +0530

    Bindable models committed.

---
+++ MSTnTAPP/Models/BindableModels/ShipmentModelView.cs
@@ -0,0 +1,121 @@
+﻿using System;
+
+namespace MSTnTAPP.Models.BindableModels
+{
+    public class ShipmentModelView:Shipment
+    {
+        public ShipmentModelView(Shipment shipment)
+        {
+            JobReferenceNumber = shipment.JobReferenceNumber;
+            JobId = shipment.JobId;
+            JobType = shipment.JobType;
+            TransportType = shipment.TransportType;
+            TransportDirection = shipment.TransportDirection;
+            ConsigneeName = shipment.ConsigneeName;
+            DateSailed = shipment.DateSailed;
+            CustomerReference = shipment.CustomerReference;
+            PlaceOfReceipt = shipment.PlaceOfReceipt;
+            VesselName = shipment.VesselName;
+            PortOfLoading = shipment.PortOfLoading;
+            PortOfDischarge = shipment.PortOfDischarge;
+            FinalDestination = shipment.FinalDestination;
+            ETA = shipment.ETA;
+            Pieces = shipment.Pieces;
+            Summary = shipment.Summary;
+            ShipperName = shipment.ShipperName;
+        }
+        //Custom
+        public string ShipmentImage
+        {
+            get
+            {
+                if (TransportType.Equals("Air"))
+                {
+                    return "plane.png";
+                }
+                else if (TransportType.Equals("Sea"))
+                {
+                    return "ship.png";
+                }
+                else if (TransportType.Equals("Land"))
+                {
+                    return "truck.png";
+                }
+                return "";
+            }
+        }
+        public string ShipmentImageOverview
+        {
+            get
+            {
+                if (TransportType.Equals("Air") && TransportDirection.Equals("Export"))
+                {
+                    return "planeright.png";
+                }
+                else if (TransportType.Equals("Air") && TransportDirection.Equals("Import"))
+                {
+                    return "planeleft.png";
+                }
+                else if (TransportType.Equals("Sea") && TransportDirection.Equals("Export"))
+                {
+                    return "shipright.png";
+                }
+                else if (TransportType.Equals("Sea") && TransportDirection.Equals("Import"))
+                {
+                    return "shipleft.png";
+                }
+                else if (TransportType.Equals("Land") && TransportDirection.Equals("Export"))
+                {
+                    return "truckright.png";
+                }
+                else if (TransportType.Equals("Land") && TransportDirection.Equals("Import"))
+                {
+                    return "truckleft.png";
+                }
+                return "";
+            }
+        }
+        public string _DateSailed
+        {
+            get
+            {
+                if (DateSailed == null)
+                {
+                    return DateTime.Now.ToString("dd MMMM yyyy");
+                }
+                else
+                {
+                    return ((DateTime)DateSailed).ToString("dd MMMM yyyy");
+                }
+            }
+        }
+        public string _ETA
+        {
+            get
+            {
+                if (ETA == null)
+                {
+                    return DateTime.Now.ToString("dd MMMM yyyy");
+                }
+                else
+                {
+                    return ((DateTime)ETA).ToString("dd MMMM yyyy");
+                }
+            }
+        }
+        public string Place_Date
+        {
+            get
+            {
+                return PlaceOfReceipt + " | " + _DateSailed;
+            }
+        }
+        public string TransportTypeName
+        {
+            get
+            {
+                return TransportType;
+            }
+        }
+    }
+}
---
+++ MSTnTAPP/Models/BindableModels/TrackingModelView.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MSTnTAPP.Models.BindingModels
+{
+    public class TrackingModelView:Tracking
+    {
+        public TrackingModelView(Tracking tracking)
+        {
+            LocationType = tracking.LocationType;
+            LocationName = tracking.LocationName;
+            OriginalEstimated = tracking.OriginalEstimated;
+            CurrentEstimated = tracking.CurrentEstimated;
+            ConfirmedActual = tracking.ConfirmedActual;
+        }
+
+        public string Original { get { try { return "Original Estimated: " + ((DateTime)OriginalEstimated).ToString("dd MMMM yyyy"); } catch (Exception e) { return ""; } } }
+        public string Current { get { try { return "Current: " + ((DateTime)CurrentEstimated).ToString("dd MMMM yyyy"); } catch (Exception e) { return ""; } } }
+        public string Actual { get { try { return "Confirmed Actual: "+ ((DateTime)ConfirmedActual).ToString("dd MMMM yyyy"); } catch (Exception e) { return ""; } } }
+        public string StatusPointer
+        {
+            get
+            {
+                if (ConfirmedActual == null)
+                {
+                    return "done.png";
+                }
+                else
+                {
+                    return "pending.png";
+                }
+            }
+        }
+    }
+}
