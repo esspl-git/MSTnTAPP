0aa729f206b10b7102eece55f91dccf6e7c11488
esspl <essplmsdn@hotmail.com>
2019-11-18 17:28:27 +0530

    iOS custom renderers committed

---
+++ MSTnTAPP.iOS/CustomRenderer/CustomPickerRenderer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using MSTnTAPP.CustomControl;
+using MSTnTAPP.iOS.CustomRenderer;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(CustomPicker), typeof(CustomPickerRenderer))]
+namespace MSTnTAPP.iOS.CustomRenderer
+{
+    public class CustomPickerRenderer : PickerRenderer, IUIPickerViewDelegate
+    {
+        IElementController ElementController => Element as IElementController;
+
+        public CustomPickerRenderer()
+        {
+
+        }
+
+        // To be uncemmented after testing is done in iOS
+
+        /*[Export("pickerView:viewForRow:forComponent:reusingView:")]
+        public UIView GetView(UIPickerView pickerView, nint row, nint component, UIView view)
+        {
+
+            UILabel label = new UILabel
+            {
+                //here you can set the style of item!!!
+
+                TextColor = UIColor.Blue,
+
+                Text = Element.Items[(int)row].ToString(),
+
+                TextAlignment = UITextAlignment.Center,
+
+            };
+            return label;
+        }*/
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Picker> e)
+        {
+            base.OnElementChanged(e);
+
+            var element = (CustomPicker)this.Element;
+
+            if (this.Control != null && this.Element != null && !string.IsNullOrEmpty(element.Image))
+            {
+                var downarrow = UIImage.FromBundle(element.Image);
+                Control.RightViewMode = UITextFieldViewMode.Always;
+                Control.RightView = new UIImageView(downarrow);
+                //UIPickerView pickerView = (UIPickerView)Control.InputView;
+                //pickerView.WeakDelegate = this;
+            }
+        }
+    }
+}
---
+++ MSTnTAPP.iOS/CustomRenderer/GradientLayoutRenderer.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+
+using CoreAnimation;
+using CoreGraphics;
+using MSTnTAPP.CustomRenderer;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using MSTnTAPP.iOS.CustomRenderer;
+
+[assembly: ExportRenderer(typeof(MSTnTAPP.CustomRenderer.GradientLayoutRenderer), typeof(MSTnTAPP.iOS.CustomRenderer.GradientLayoutRenderer))]
+
+namespace MSTnTAPP.iOS.CustomRenderer 
+{
+    public class GradientLayoutRenderer : VisualElementRenderer<StackLayout>
+    {
+        public override void Draw(CGRect rect)
+        {
+            base.Draw(rect);
+            MSTnTAPP.CustomRenderer.GradientLayoutRenderer layout = (MSTnTAPP.CustomRenderer.GradientLayoutRenderer)Element;
+
+            CGColor[] colors = new CGColor[layout.Colors.Length];
+
+            for (int i = 0, l = colors.Length; i < l; i++)
+            {
+                colors[i] = layout.Colors[i].ToCGColor();
+            }
+
+            var gradientLayer = new CAGradientLayer();
+
+            switch (layout.Mode)
+            {
+                default:
+                case GradientColorStackMode.ToRight:
+                    gradientLayer.StartPoint = new CGPoint(0, 0.5);
+                    gradientLayer.EndPoint = new CGPoint(1, 0.5);
+                    break;
+                case GradientColorStackMode.ToLeft:
+                    gradientLayer.StartPoint = new CGPoint(1, 0.5);
+                    gradientLayer.EndPoint = new CGPoint(0, 0.5);
+                    break;
+                case GradientColorStackMode.ToTop:
+                    gradientLayer.StartPoint = new CGPoint(0.5, 1);
+                    gradientLayer.EndPoint = new CGPoint(0.5, 0);
+                    
+                    break;
+                case GradientColorStackMode.ToBottom:
+                    gradientLayer.StartPoint = new CGPoint(0.5, 0);
+                    gradientLayer.EndPoint = new CGPoint(0.5, 0.75);
+                    
+                    break;
+                case GradientColorStackMode.ToTopLeft:
+                    gradientLayer.StartPoint = new CGPoint(1, 0);
+                    gradientLayer.EndPoint = new CGPoint(0, 1);
+                    break;
+                case GradientColorStackMode.ToTopRight:
+                    gradientLayer.StartPoint = new CGPoint(0, 1);
+                    gradientLayer.EndPoint = new CGPoint(1, 0);
+                    break;
+                case GradientColorStackMode.ToBottomLeft:
+                    gradientLayer.StartPoint = new CGPoint(1, 1);
+                    gradientLayer.EndPoint = new CGPoint(0, 0);
+                    break;
+                case GradientColorStackMode.ToBottomRight:
+                    gradientLayer.StartPoint = new CGPoint(0, 0);
+                    gradientLayer.EndPoint = new CGPoint(1, 1);
+                    break;
+            }
+
+            gradientLayer.Frame = rect;
+            gradientLayer.Colors = colors;
+
+            NativeView.Layer.InsertSublayer(gradientLayer, 0);
+        }
+    }
+}
